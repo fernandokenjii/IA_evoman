89700d1cfcbd19094d7df044215e2be6b2763253
LJC-DB <leonardojcarmo@hotmail.com>
2019-11-19 09:04:15 -0300

    refactor: Simplify action selection

--- our_controller.py
+++ our_controller.py
@@ -20,38 +20,7 @@ class player_controller(Controller):
         threshold = 0.5
         output = controller.model.predict(inputs)
         output=output[0]
-        
-        if output[0] > threshold:
-            left = 1
-        else:
-            left = 0
-
-        if output[1] > threshold:
-            right = 1
-        else:
-            right = 0
-
-        if output[2] > threshold:
-            jump = 1
-        else:
-            jump = 0
-
-        if output[3] > threshold:
-            # if inputs[0][0] > 0 and inputs[0][2] == -1:
-            #     left=1
-            #     right=0
-            # elif inputs[0][0] < 0 and inputs[0][2] == 1:
-            #     left=0
-            #     right=1
-            shoot = 1
-        else:
-            shoot = 0
-
-        if output[4] > threshold:
-            release = 1
-        else:
-            release = 0
-
-        return [left, right, jump, shoot, release]
+        actions = np.where(output>threshold, 1, 0)
+        return actions
     def fit_scale(self):
         self.scale.fit(self.x_train)
